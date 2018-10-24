bbe83ccdcd0bc97916e99d80e7cd510689262d25
System Administrator <root@Sachins-MacBook-Pro.local>
2018-10-23 23:49:41 -0700

    update

--- IOS11CollectionViewControllerTutorial/IOS11CollectionViewControllerTutorial/CollectionViewController.swift
+++ IOS11CollectionViewControllerTutorial/IOS11CollectionViewControllerTutorial/CollectionViewController.swift
@@ -37,14 +37,40 @@ class CollectionViewController: UICollectionViewController {
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // 2
-        return 100
+        return 3
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        
+       
+        
     
         // Configure the cell
         // 3
+        
+      //  cell.oddcellTitleLabel.frame = CGRect(x: 70, y: 110, width: 90, height: 50)
+        
+//        if let layout = collectionview2.collectionViewLayout as? UICollectionViewFlowLayout { layout.scrollDirection = .horizontal }
+//        self.collectionview2.isPagingEnabled = true
+//        self.collectionview2.alwaysBounceHorizontal = true
+        
+        print (collectionView.frame.size.width)
+//
+        let height = collectionView.frame.size.width/3
+        let width = collectionView.frame.size.width/3
+        
+        cell.frame.size.width = width
+        cell.frame.size.height = height
+        
+        print (height, width);
+        
+        print(collectionView.bounds.height)
+       print(collectionView.bounds.width)
+
+        
+        //cell.frame.size.width = self.view.frame.width / 3
+        //cell.frame.size.height = self.view.frame.height / 4
         cell.backgroundColor = cellColor ? UIColor.red : UIColor.blue
         cellColor = !cellColor
     
